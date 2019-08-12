31691df7e8dd157aa5f1b9fcf445e0d398d09e00
corbinq27 <corbinjohnson@me.com>
2019-08-11 20:10:14 -0500

    Create BowlingGameTest.py
    The tests first.

---
+++ BowlingGameTest.py
@@ -0,0 +1,42 @@
+import unittest
+from Game import Game
+
+class BowlingGameTest(unittest.TestCase):
+
+    def setUp(self):
+        self.g = Game()
+
+    def test_gutter_game(self):
+        self.rollMany(20, 0)
+
+        self.assertEquals(0, self.g.return_score())
+
+    def rollMany(self, pins, roll):
+        for _ in range(0, pins):
+            self.g.roll(roll)
+
+    def test_all_ones(self):
+        self.rollMany(20, 1)
+        self.assertEquals(20, self.g.return_score())
+
+    def test_one_spare(self):
+        self.rollSpare()
+        self.g.roll(3)
+        self.rollMany(17,0)
+        self.assertEquals(16, self.g.return_score())
+
+    def test_one_strike(self):
+        self.g.roll(10) #strike
+        self.g.roll(3)
+        self.g.roll(4)
+        self.rollMany(16, 0)
+        self.assertEquals(24, self.g.return_score())
+
+    def test_perfect_game(self):
+        self.rollMany(12, 10)
+        self.assertEquals(300, self.g.return_score())
+
+
+    def rollSpare(self):
+        self.g.roll(5)
+        self.g.roll(5)  # spare
